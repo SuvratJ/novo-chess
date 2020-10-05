65f89348526d766139dd9cbf757bda62e7fb68eb
Suvrat Jain <suvrat_jain@outlook.com>
2020-10-05 13:57:23 +0530

    Added routing to the application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css'
+
+import Navbar from "./components/navbar.component.js";
+import Home from "./components/home.component";
+import Game from "./components/game.component";
+import Create from "./components/create.component";
+import Browse from "./components/browse.component";
+import Login from "./components/login.component";
+import Settings from "./components/settings.component";
 
 function App() {
   return (
-    <div className="App">
-      Hello, World
-    </div>
+    <Router>
+      <Navbar />
+      <Route path="/" exact component={Home} />
+      <Route path="/play" component={Game} />
+      <Route path="/create" component={Create} />
+      <Route path="/browse" component={Browse} />
+      <Route path="/login" component={Login} />
+      <Route path="/settings" component={Settings} />
+    </Router>
   );
 }
 
---
+++ src/components/navbar.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Navbar(){
+    return (
+        <nav class="navbar">
+            <ul>
+            <li>abc</li>
+            <li>pqr</li>
+            <li>xyz</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
